fff92bb78400a161055c085e5303465c1cb8c3bd
Amadeusz Majcher <amadeuszmajcher@gmail.com>
2025-09-02 20:30:17 +0200

    adding the hero component

--- front-end/src/views/MainPage/MainPage.jsx
+++ front-end/src/views/MainPage/MainPage.jsx
@@ -1,4 +1,10 @@
+import { Hero } from "../../components/Hero/Hero";
 import { Layout } from "../../components/Layout/Layout";
+import COVER_PHOTO from "../../assets/etui.png";
 export function MainPage() {
-  return <Layout />;
+  return (
+    <Layout>
+      <Hero heroImage={COVER_PHOTO} />
+    </Layout>
+  );
 }
