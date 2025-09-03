d72140accc7995a55de8a7e00b8a4553d4ba8e26
Amadeusz Majcher <amadeuszmajcher@gmail.com>
2025-09-03 21:54:31 +0200

    adding a product details page and a component displaying a product photo gallery

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,12 +3,12 @@ import "./styles/globals.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ProductsList } from "./views/ProductsList/ProductsList.jsx";
+import { ProductDetails } from "./views/ProductDetails/ProductDetails.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <ProductsList />,
+    element: <ProductDetails />,
   },
 ]);
 
