e4f5d6b2e8d1677c9b806e370dcbcf8f7e54f05a
Amadeusz Majcher <amadeuszmajcher@gmail.com>
2025-09-04 01:26:09 +0200

    adding pagination

--- front-end/src/api/productListLoader.js
+++ front-end/src/api/productListLoader.js
@@ -2,12 +2,17 @@ import { redirect } from "react-router-dom";
 import { BACK_END_URL } from "../constants/api";
 import { CATEGORIES } from "../constants/categories";
 
-export function productListLoader({ params: { type, category, subcategory } }) {
+export function productListLoader({
+  params: { type, category, subcategory },
+  request,
+}) {
+  const pageUrl = new URL(request.url);
+  const page = pageUrl.searchParams.get("page") || 1;
   const foundCategory = CATEGORIES.find((c) => c.path === category);
   const foundType = type;
 
   if (foundType && foundCategory) {
-    let url = `${BACK_END_URL}/products/?gender=${foundType}&category=${category}`;
+    let url = `${BACK_END_URL}/products/?type=${foundType}&category=${category}`;
 
     if (subcategory) {
       const foundSubcategory = foundCategory.subcategories.find(
@@ -20,7 +25,20 @@ export function productListLoader({ params: { type, category, subcategory } }) {
       }
     }
 
-    return fetch(url);
+    url = `${url}&_limit=8&_page=${page}`;
+
+    return fetch(url).then((response) => {
+      const numberOfPages = Math.ceil(
+        Number(response.headers.get("X-Total-Count")) / 8
+      );
+
+      return response.json().then((products) => {
+        return {
+          products,
+          numberOfPages,
+        };
+      });
+    });
   } else {
     redirect("/phone");
   }
