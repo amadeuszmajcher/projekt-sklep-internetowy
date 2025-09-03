50c32e0cbf74c96e2da64a7d48d9734da4f1fab6
Amadeusz Majcher <amadeuszmajcher@gmail.com>
2025-09-03 22:16:44 +0200

    adding a component with product details

--- front-end/src/views/ProductDetails/ProductDetails.jsx
+++ front-end/src/views/ProductDetails/ProductDetails.jsx
@@ -3,6 +3,7 @@ import { FlexContainer } from "../../components/FlexContainer/FlexContainer";
 import { ExpandableMenu } from "../../components/ExpandableMenu/ExpandableMenu";
 import { Breadcrumbs } from "../../components/Breadcrumbs/Breadcrumbs";
 import { Photos } from "../../components/Photos/Photos";
+import { Details } from "../../components/Details/Details";
 export function ProductDetails() {
   const product = {
     id: 1,
@@ -27,9 +28,12 @@ export function ProductDetails() {
     <Layout>
       <FlexContainer>
         <ExpandableMenu />
-        <div>
+        <div style={{ width: "100%" }}>
           <Breadcrumbs />
-          <Photos product={product} />
+          <FlexContainer>
+            <Photos product={product} />
+            <Details product={product} />
+          </FlexContainer>
         </div>
       </FlexContainer>
     </Layout>
