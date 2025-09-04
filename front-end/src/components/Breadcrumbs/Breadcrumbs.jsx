e44f08cb1adae84475bff9e75f34b25653f66508
Amadeusz Majcher <amadeuszmajcher@gmail.com>
2025-09-04 11:05:52 +0200

    downloading breadcrumbs from BE

--- front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
+++ front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,28 +1,39 @@
 import styles from "./Breadcrumbs.module.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import ARROW_ICON from "../../assets/arrow.svg";
+import { CATEGORIES, TYPES } from "../../constants/categories";
 export function Breadcrumbs() {
+  const { type, category, subcategory } = useParams();
+
+  const foundType = TYPES.find((t) => t.path === type);
+  const foundCategory = CATEGORIES.find((c) => c.path === category);
+
   const breadcrumbs = [
     {
-      categoryName: "Telefon",
-      path: "telefon",
+      categoryName: foundType.categoryName,
+      path: `/${foundType.path}`,
     },
     {
-      categoryName: "Etui",
-      path: "etui",
-    },
-    {
-      categoryName: "iPhone 17 Pro Max",
-      path: "iphone-17-pro-max",
+      categoryName: foundCategory.categoryName,
+      path: `/${foundType.path}/${foundCategory.path}`,
     },
   ];
+  if (subcategory) {
+    const foundSubcategory = foundCategory.subcategories.find(
+      (sc) => sc.path === subcategory
+    );
+    breadcrumbs.push({
+      categoryName: foundSubcategory.categoryName,
+      path: `/${foundType.path}/${foundCategory.path}/${foundSubcategory.path}`,
+    });
+  }
 
   return (
     <ul className={styles.breadcrumbs}>
       {breadcrumbs.map((breadcrumb) => {
         return (
           <li key={breadcrumb.path}>
-            <NavLink to={breadcrumb.path}>
+            <NavLink end to={breadcrumb.path}>
               {breadcrumb.categoryName}
               <img src={ARROW_ICON} />
             </NavLink>
